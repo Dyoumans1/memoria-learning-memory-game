e27949865d3ca86ff19c45f85906c75c491e7db0
Dyoumans <derrikyoumans416@gmail.com>
2025-02-04 17:47:35 -0500

    Reworked the cards have data-value and needed functions

--- app.js
+++ app.js
@@ -4,32 +4,98 @@ const cardCharacters = {
 
     romanji: ["a", "i", "o", "u", "e", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya" , "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo", "n"],
 };
+
+let matchedCards = 0;
+let gameStarted = false;
+let firstCard = null;
+let secondCard = null;
+let timer;
+let timeRemaining = 90;
+
+
+
+
 // card pairs
 
 const cardPairs = () => {
     const pairs = [];
-    for(let idx = 0; idx < cardCharacters.hiragana.length; idx++) {
-        pairs.push(cardCharacters.hiragana[idx], cardCharacters.romanji[idx]);
+    let indexes = [];
+    
+    while ( pairs.length < 16) {
+        let randIndex = Math.floor(Math.random() * cardCharacters.hiragana.length);
+        if(!indexes.includes(randIndex)) {
+            indexes.push(randIndex);
+            pairs.push(cardCharacters.hiragana[randIndex], cardCharacters.romanji[randIndex]);
+        }
     }
-    return pairs;
+    // for(let idx = 0; idx < cardCharacters.hiragana.length; idx++) {
+    //     pairs.push(cardCharacters.hiragana[idx], cardCharacters.romanji[idx]);
+    // }
+    return pairs.sort(() => Math.random() - 0.5); //should choose pairs randomly
 };
+
+
 // console.log(cardPairs());
 
-//shuffleCards??
 
-Math.floor(Math.random() * 9 );
-
-console.log()
 
 //move card to grid
+
+const placeCards = (pairs) => {
+    
+ const gameDeck = document.querySelector('.cards');
+    
+    for (let idx = 0; idx < gameDeck.length; idx++) {
+        gameDeck[idx].dataset.value = pairs[idx];
+        gameDeck[idx].textContent = '';
+    }
+};
+
+// start the game
+
+document.getElementById('startButton'). addEventListener('click', () => {
+    const pairs = cardPairs();
+    placeCards(pairs);
+    startTimer();
+})
+
+
 //flipping the cards
+
+const flipCard = (card) => {
+    if (gameStarted === null || card === firstCard || card.classList.contains(`flipped`))
+     return;
+
+    card.classList.add(`flipped`);
+    card.textContent = card.dataset.value;
+
+
+    if (firstCard === null) {
+        firstCard = card;
+    } else {
+        secondCard = card;
+    }
+
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
 //buttons
 //timer
-let timer;
-let timeRemaining = 90;
 
 const startTimer = () => {
-    timeRemaining =90;
+    
+
     document.querySelector(`.time`).innerText = timeRemaining;
     timer = setInterval(() => {
         if (timeRemaining > 0) {
