05ec6b13013d7bb84626e99859db5c256271256c
Dyoumans <derrikyoumans416@gmail.com>
2025-02-03 20:11:17 -0500

    Added characters

---
+++ app.js
@@ -0,0 +1,9 @@
+const cardCharacters = {
+    hiragana: ["あ", "い", "お", "う", "え", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や" , "ゆ", "よ", "ら", "り", "る", "れ", "ろ", "わ", "を", "ん"],
+
+
+    romanji: ["a", "i", "o", "u", "e", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya" , "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo", "n"],
+};
+
+console.log(cardCharacters.romanji.length)
+console.log(cardCharacters.hiragana.length)
